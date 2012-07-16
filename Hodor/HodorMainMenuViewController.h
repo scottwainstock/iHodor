d13d4e7dbc30260355e284f02d7612e290f36829
Scott Wainstock <scott.wainstock@gmail.com>
2012-07-15 19:33:41 -0700

    drew spec main menu (missing iHodor logo and animals images)

--- Hodor/HodorMainMenuViewController.h
+++ Hodor/HodorMainMenuViewController.h
@@ -1,10 +1,9 @@
-#import <UIKit/UIKit.h>
 #import "HodorViewController.h"
 
-@interface HodorMainMenuViewController : HodorViewController {
-}
+@interface HodorMainMenuViewController : HodorViewController
 
-- (IBAction)goButtonPressed:(id)sender;
+- (IBAction)chatButtonPressed:(id)sender;
+- (IBAction)poseButtonPressed:(id)sender;
 - (IBAction)aboutButtonPressed:(id)sender;
 - (IBAction)helpButtonPressed:(id)sender;
 
--- Hodor/HodorMainMenuViewController.m
+++ Hodor/HodorMainMenuViewController.m
@@ -1,36 +1,26 @@
 #import "HodorMainMenuViewController.h"
 #import "AboutViewController.h"
 #import "HelpViewController.h"
+#import "HodorTalkerViewController.h"
+#import "HodorAndBranTalkerViewController.h"
 
 @implementation HodorMainMenuViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-        
-    UISwitch *talkerType = [[UISwitch alloc] initWithFrame:CGRectZero];
-    [talkerType addTarget:self action:@selector(toggleTalker:) forControlEvents:UIControlEventValueChanged];
-    [talkerType setOn:[[NSUserDefaults standardUserDefaults] boolForKey:@"branType"]];
-        
-    [self.view addSubview:talkerType];
-    
-    [talkerType release];
-}
 
 - (IBAction)toggleTalker:(id)sender {
     BOOL branType = [[NSUserDefaults standardUserDefaults] boolForKey:@"branType"];
     [[NSUserDefaults standardUserDefaults] setBool:!branType forKey:@"branType"];
 }
 
-- (IBAction)goButtonPressed:(id)sender {
-    NSString *viewControllerClassName = [[NSUserDefaults standardUserDefaults] boolForKey:@"branType"] ? 
-        @"HodorAndBranTalkerViewController" :
-        @"HodorTalkerViewController"
-    ;
-            
-    id viewController = [[NSClassFromString(viewControllerClassName) alloc] init];
-    [self.navigationController pushViewController:viewController animated:YES];
-    
-    [viewController release];
+- (IBAction)chatButtonPressed:(id)sender {
+    HodorTalkerViewController *hodorViewController = [[HodorTalkerViewController alloc] init];
+    [self.navigationController pushViewController:hodorViewController animated:YES];
+    [hodorViewController release];
+}
+
+- (IBAction)poseButtonPressed:(id)sender {
+    HodorAndBranTalkerViewController *hodorAndBranTalkerViewController = [[HodorAndBranTalkerViewController alloc] init];
+    [self.navigationController pushViewController:hodorAndBranTalkerViewController animated:YES];
+    [hodorAndBranTalkerViewController release];
 }
 
 - (IBAction)aboutButtonPressed:(id)sender {
